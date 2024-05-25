dfac6ab2d1c14014640393ccd55c6312580573e1
Tigran Karakhanyan <tigran@uni.minerva.edu>
2024-05-24 18:45:36 -0700

    update main page + title

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
-import Image from "next/image";
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main>
-      <div>
-        I often get asked. What The Fuck are you doing with your life? 
-
-        This website is meant as a general answer to that question. 
-
-        More to come soon.
+      <div className="p-6 bg-white rounded-lg">
+        <p className="mb-4 text-lg font-semibold text-gray-800">
+          I often get asked, what the fuck are you doing with your life?
+        </p>
+        <p className="mb-4 text-gray-600">
+          This website is meant as a general answer to that question.
+        </p>
+        <p className="mb-4 text-gray-600">
+          More to come soon.
+        </p>
+        <h2 className="mb-4 text-xl font-bold">Projects 👇</h2>
+        <Link href="/projects/zentweet" className="text-blue-500 hover:underline">
+          ZenTweet
+        </Link>
       </div>
     </main>
   );
